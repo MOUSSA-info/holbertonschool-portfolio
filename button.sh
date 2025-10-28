59aafb80c03b22f0b05c13cb78a4eef3f05264e2
boumy777 <brandondialuta@gmail.com>
2025-10-28 12:06:16 +0100

    Add terminal GET STARTED button script

---
+++ button.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RED='\033[41m'   # fond rouge
+WHITE='\033[97m' # texte blanc
+BOLD='\033[1m'   # texte en gras
+RESET='\033[0m'  # réinitialisation
+
+echo -e "\n"
+echo -e "          ${RED}${WHITE}${BOLD}   GET STARTED   ${RESET}"
+echo -e "\n"
+

c96373fadc6ca8acdc8e79075355d313adbda324
boumy777 <brandondialuta@gmail.com>
2025-11-08 14:28:32 +0100

    frontend

--- MYGLOWSKILLS-frontend/src/App.js
+++ MYGLOWSKILLS-frontend/src/App.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import api from './services/api';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Projects from './pages/Projects';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    api.get('/items') // adapte le endpoint selon ton backend
-      .then(response => setData(response.data))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
-    <div>
-      <h1>Mes données :</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
-
